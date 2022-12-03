3bb8ab313605a549b212a159a243761132abbf67
Kassim Zaid <81573513+kzaid0767@users.noreply.github.com>
2022-12-03 08:53:55 -0500

    text top and bottom added

--- src/Meme.js
+++ src/Meme.js
@@ -5,25 +5,44 @@ import React, {useState} from 'react'
 
 function Meme() {
 
-    const [webs, setWebs] = useState('')
-    // './images/test.jpg'
-    let pic = `${memesData.data.memes[Math.floor(Math.random() * memesData.data.memes.length)].url}`
-    function handleClick(){
-        const memesArray = memesData.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const picSource = memesArray[randomNumber].url
-        setWebs(picSource)
-        // pic=webs
+    const [meme, setMeme] = useState({topText:'',bottomText:'', randomImage:'http://i.imgflip.com/1bij.jpg'})
+    const [allData, setAllData]= useState(memesData)
+
+    let pic = `${meme.randomImage}`
+    function handleClick(e){
+        e.preventDefault()
+        const imageUrl = allData.data.memes[Math.floor(Math.random()*allData.data.memes.length)].url
+        setMeme(prevState=>({...prevState, randomImage:imageUrl}))
+    }
+    // console.log(meme)
+    function handleChange(e){
+        e.preventDefault()
+        const {name, value} = e.target
+        setMeme(prevState => (
+            {...prevState, [name]:value}
+        ))
     }
 
-  return (
+    return (
     <div className='meme'>
         <form>
-            <input className='input-fields' type={'text'} />
-            <input className='input-fields' type={'text'} />
+            <input className='input-fields' 
+                onChange={handleChange} 
+                name='topText' 
+                value={meme.topText}  
+                type={'text'} />
+            <input className='input-fields' 
+                onChange={handleChange} 
+                name='bottomText' 
+                value={meme.bottomText} 
+                type={'text'} />
             <button onClick={handleClick}>Get a new meme image 🖼️</button>
         </form>
-        <img className='main-pic' src={webs?webs:pic} alt={'meme face showing'} />
+        <div className='image-container'>
+            <h1 className='top meme-text'>{meme.topText}</h1>
+            <img className='main-pic' src={pic} alt={'meme face showing'} />
+            <h1 className='bottom meme-text'>{meme.bottomText}</h1>
+        </div>
     </div>
   )
 }
